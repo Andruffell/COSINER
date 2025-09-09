cc5088e93e13442bd48b1c32d0bf54261c13e7a4
Andrea Vignali <an.vignali@icloud.com>
2025-09-09 17:13:57 +0200

    biobert bug fix

--- models/download_models_from_hf.py
+++ models/download_models_from_hf.py
@@ -7,7 +7,7 @@ bert_tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 bert_tokenizer.save_pretrained("./models/BERT/TOKENIZER")
 
 biobert = AutoModel.from_pretrained("dmis-lab/biobert-v1.1")
-bert.save_pretrained("./models/BioBERT/", from_pt = True)
+biobert.save_pretrained("./models/BioBERT/", from_pt = True)
 
 bert_tokenizer = AutoTokenizer.from_pretrained("dmis-lab/biobert-v1.1")
 bert_tokenizer.save_pretrained("./models/BioBERT/TOKENIZER")
